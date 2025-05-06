72d6f1147091991197ac6ba8a441af2018ae9d4a
isabellyccamargo <camargoisa114@gmail.com>
2025-05-05 23:05:50 -0300

    Atividade para treinar

--- correcaoexerciciosprova.js
+++ correcaoexerciciosprova.js
@@ -29,7 +29,7 @@ console.log("A media dos salarios foi " + media);
 console.log("A menor idade foi: " + menorIdade);
 console.log("A maior idade foi " + maiorIdade);
 console.log("Teve " + qtdMulheresSal400MenorIdade + "mulheres com salario de ate 400, menores de 18.");
-*/
+
 
 let qtdCDS= prompt("Informe a quantidade de Cds: ");
 let precoTabua = prompt("Informe o preço da tabua: ");
@@ -48,4 +48,36 @@ console.log("precisou de " + qtdTabuasCompletas + " de tabuas.");
 console.log("Com o custo de " + precoTabua + "$ por tabua e " + precoSuporte + "$ por suporte" );
 precoTabua= qtdTabuasCompletas*precoTabua;
 precoSuporte= (qtdTabuasCompletas*precoSuporte)*2;
-console.log("Com custo total de " + (precoTabua + precoSuporte));
+console.log("Com custo total de " + (precoTabua + precoSuporte));*/
+
+
+let a = parseInt(prompt("Qual a quantidade de itens?"));
+let b = Number(prompt("Qual o valor de cada caixa?"));
+let c = Number(prompt("Qual o valor de cada palete?"));
+
+console.log(calculaCustoTotalArmazenagem(a, b, c));
+
+function calculaCustoTotalArmazenagem(qtdTotalItens, precoCaixa, precoPalete) {
+
+    let qtdCaixa = 0;
+    
+    if (qtdTotalItens % 10 == 0) {
+        qtdCaixa = qtdTotalItens / 10;
+    } else {
+        qtdCaixa = (qtdTotalItens / 10) + 1;
+    }
+
+    let qtdPalete = 0;
+
+    if (qtdCaixa % 5 == 0) {
+        qtdPalete = qtdCaixa / 5;
+    } else {
+        qtdPalete = (qtdCaixa / 5) + 1;
+    }
+
+    let precoTotalCaixa = qtdCaixa * precoCaixa;
+    let precoTotalPalete = qtdPalete * precoPalete;
+
+    return precoTotalCaixa + precoTotalPalete;
+}
+
